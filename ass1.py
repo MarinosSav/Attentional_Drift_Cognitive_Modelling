7130fa567b1c6172fb7da987e4237a2ca9e346a2
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2022-11-21 10:24:30 +0100

    Add files via upload

--- ass1.py
+++ ass1.py
@@ -1,6 +1,8 @@
 import numpy
 import time
+import matplotlib.pyplot as plt
 
+TYPES = {"fast", "middle", "slow"}
 
 def start():
 
@@ -9,27 +11,42 @@ def start():
     return round((time.time() - start_time) * 1000)
 
 
-def perceptualstep():
+def perceptualstep(type="middle"):
 
-    t_percept = 100
+    if type == "fast":
+        t_percept = 50
+    elif type == "slow":
+        t_percept = 200
+    else:
+        t_percept = 100
 
     start_time = time.time()
 
     return round((time.time() - start_time) * 1000) + t_percept
 
 
-def cognitivestep():
+def cognitivestep(type="middle"):
 
-    t_cog = 70
+    if type == "fast":
+        t_cog = 25
+    elif type == "slow":
+        t_cog = 170
+    else:
+        t_cog = 70
 
     start_time = time.time()
 
     return round((time.time() - start_time) * 1000) + t_cog
 
 
-def motorstep():
+def motorstep(type="middle"):
 
-    t_motor = 70
+    if type == "fast":
+        t_motor = 30
+    elif type == "slow":
+        t_motor = 100
+    else:
+        t_motor = 70
 
     start_time = time.time()
 
@@ -45,9 +62,33 @@ def example1():
 
     return total_time
 
+def example2(completeness="extremes"):
+
+    time_list = []
+    if completeness == "extremes":
+        for type in TYPES:
+            total_time = start()
+            total_time += perceptualstep(type=type)
+            total_time += cognitivestep(type=type)
+            total_time += motorstep(type=type)
+            time_list.append(total_time)
+    elif completeness == "all":
+        for type_i in TYPES:
+            for type_j in TYPES:
+                for type_z in TYPES:
+                    total_time += perceptualstep(type=type_i)
+                    total_time += cognitivestep(type=type_j)
+                    total_time += motorstep(type=type_z)
+                    time_list.append(total_time)
+        plt.boxplot(time_list)
+
+
+    return time_list
+
+
 def main():
 
-    print(example1())
+    print(example2())
 
 
 if __name__ == '__main__':
