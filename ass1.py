9cceb1127ca04cad311e0ff9b8fbe2012bb8a016
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2022-11-21 09:38:16 +0100

    Add files via upload

--- ass1.py
+++ ass1.py
@@ -30,6 +30,15 @@ def motorstep():
     return round((time.time() - start_time) * 1000)
 
 
+def example1():
+
+    total_time = start()
+    total_time += perceptualstep()
+    total_time += cognitivestep()
+    total_time += motorstep()
+
+    return total_time
+
 def main():
     pass
 
