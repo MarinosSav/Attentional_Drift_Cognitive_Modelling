56bff6605df2938458a4f7e73c78a58e4a6a989d
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2022-11-21 09:32:27 +0100

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,33 @@
+import numpy
+import time
+
+
+def start():
+
+    start_time = time.time()
+    for i in range(3):
+        time.sleep(1)
+
+    return round((time.time() - start_time) * 1000)
+
+def perceptualstep():
+    pass
+
+    return time
+
+def cognitivestep():
+    pass
+
+    return time
+
+def motorstep():
+    pass
+
+    return time
+
+def main():
+
+    print(start())
+
+if __name__ == '__main__':
+    main()
